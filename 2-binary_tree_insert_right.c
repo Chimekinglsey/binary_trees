7b09aa63648a2de66bbc2c25d3f55594cc16f0ed
chimekinglsey <onlinekingsley@gmail.com>
2023-05-31 08:32:36 +0000

    revamped

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *insertRight;
 
 	insertRight = malloc(sizeof(binary_tree_t));
-	if (insertRight == NULL || parent == NULL)
+	if (insertRight == NULL || parent == NULL || value == '\0')
 		return (NULL);
 	insertRight->n = value;
 	insertRight->left = NULL;
