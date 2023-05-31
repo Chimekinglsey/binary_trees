56db5d477c8d115eec3c8844989c85a44da321f1
chimekinglsey <onlinekingsley@gmail.com>
2023-05-31 06:29:24 +0000

    New commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		binary_tree_t *temp = parent->left;
+
 		parent->left = insert;
 		parent->left->left = temp;
 	}
