98c9f338e07894da8c1cd5ce7f06c873d65a9845
chimekinglsey <onlinekingsley@gmail.com>
2023-06-02 09:19:54 +0000

    iterative height rejected, recursive used

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,36 +10,38 @@ size_t binary_tree_height(const binary_tree_t *tree)
 /*	const binary_tree_t *counter = tree;*/
 	size_t countleft = 0, countright = 0, count = 0;
 
-	if (tree == NULL)
-		return (0);
-	for(countleft = 0; tree != NULL; countleft++)
-	{
-			if (tree->left == NULL && tree->right == NULL)
-					return (0);
-			tree = tree->left;
-	}
-	for(countright = 0; tree != NULL; countright++)
-	{
-			if (tree->left == NULL && tree->right == NULL)
-					return (0);
-			tree = tree->right;
-	}
-	if (countleft > countright)
-		count = countleft;
-	else
-		count = countright;
+/*
+*	if (tree == NULL)
+*		return (0);
+*	for(countleft = 0; tree != NULL; countleft++)
+*	{
+*			if (tree->left == NULL && tree->right == NULL)
+*					return (0);
+*			tree = tree->left;
+*	}
+*	for(countright = 0; tree != NULL; countright++)
+*	{
+*			if (tree->left == NULL && tree->right == NULL)
+*					return (0);
+*			tree = tree->right;
+*	}
+*	if (countleft > countright)
+*		count = countleft;
+*	else
+*		count = countright;
+*
+*	return (count);
+*/
 
-	return (count);
-
-/*	if (tree->left)
-	countleft += binary_tree_height(tree->left);
+	if (tree->left)
+	countleft = 1 + binary_tree_height(tree->left);
 
 	if (tree->right)
-	countright += binary_tree_height(tree->right);
+	countright = 1 + binary_tree_height(tree->right);
 
 	if (countleft > countright)
 		count =  countleft;
 	else
 		count = countright;
-return ((count));*/
+return ((count));
 }
