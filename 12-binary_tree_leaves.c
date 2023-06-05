77523a3bc575422d9896d4824b2ecd8c37910bd6
chimekinglsey <onlinekingsley@gmail.com>
2023-06-05 12:34:31 +0000

    new commit

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,19 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - function that counts the leaves in a binary tree
- * @tree: pointer node binary tree
- * Return: Number the leaves in a binary tree
- */
+* binary_tree_leaves - a function that counts the leaves in a binary tree
+* @tree: pointer to the root node
+* Return: unsigned int value of total leaves
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t countleft = 0, countright = 0;
 
 	if (!tree)
 		return (0);
-	if (!tree->left && !tree->right)
-		leaves += 1;
-	leaves += binary_tree_leaves(tree->left);
-	leaves += binary_tree_leaves(tree->right);
-	return (leaves);
+
+/*
+*	while (tree->left)
+*	{
+*		if (!tree->left)
+*			countleft++;
+*		if (!tree->right)
+*			countright++;
+*		if (!tree->left && !tree->right)
+*			total += 2;
+*		tree = tree->left;
+*	}
+*	while (tree->right)
+*	{
+*		if (!tree->left)
+*			countleft++;
+*		if (!tree->right)
+*			countright++;
+*		if (!tree->left && !tree->right)
+*			total += 2;
+*		tree = tree->right;
+*	}
+*	return (countright + countleft + 1);
+*/
+if (!tree->left && !tree->right)
+	return (1);
+countleft =  binary_tree_leaves(tree->left);
+countright =  binary_tree_leaves(tree->right);
+return (countright + countleft);
 }
